18a7b2bbad32f565a075fc077d6e645df6127da9
Duong <duongpq.22bi13116@usth.edu.com>
2024-09-03 20:45:10 +0700

    Labwork1,2,3,4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -6,15 +6,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class ForecastFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(Color.parseColor("#20FF0000"));
+//        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+//        view.setBackgroundColor(Color.parseColor("#20FF0000"));
+//
+//        return view;
 
-        return view;
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView textView = new TextView(getContext());
+        textView.setText("Thursday");
+
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.cloudy);
+
+        linearLayout.addView(textView);
+        linearLayout.addView(imageView);
+
+        return linearLayout;
     }
 }
