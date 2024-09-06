4aebe3e5a628e5a6a1b8adad834f0ce7bb246bba
Duong <duongpq.22bi13116@usth.edu.com>
2024-09-06 12:54:30 +0700

    Labwork 1,2,3,4,5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -16,23 +16,25 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
 //        view.setBackgroundColor(Color.parseColor("#20FF0000"));
+
+       return view;
+
+
+////        //Labwork 4
+////        LinearLayout linearLayout = new LinearLayout(getContext());
+////        linearLayout.setOrientation(LinearLayout.VERTICAL);
+////
+////        TextView textView = new TextView(getContext());
+////        textView.setText("Thursday");
+////
+////        ImageView imageView = new ImageView(getContext());
+////        imageView.setImageResource(R.drawable.cloudy);
+////
+////        linearLayout.addView(textView);
+////        linearLayout.addView(imageView);
 //
-//        return view;
-
-        LinearLayout linearLayout = new LinearLayout(getContext());
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        TextView textView = new TextView(getContext());
-        textView.setText("Thursday");
-
-        ImageView imageView = new ImageView(getContext());
-        imageView.setImageResource(R.drawable.cloudy);
-
-        linearLayout.addView(textView);
-        linearLayout.addView(imageView);
-
-        return linearLayout;
+//        return linearLayout;
     }
 }
